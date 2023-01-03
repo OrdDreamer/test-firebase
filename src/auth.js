9ed71d3c8520541979545ba765c96a01e4c57bc7
Roman Snitsarenko <59483945+OrdDreamer@users.noreply.github.com>
2023-01-04 01:00:02 +0200

    Upd Auth

--- src/auth.js
+++ src/auth.js
@@ -1,31 +1,23 @@
 import { getAuth, signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
 
-const callbacks = new Set();
-export function addListenerOnAuth(callback) {
-  if (typeof callback === "function") {
-    callbacks.add(callback);
-  }
-}
-
-function onAuth(user) {
-  for (const callback of callbacks) {
-    callback(user);
-  }
-}
-
 export function userSignIn() {
   signInWithPopup(getAuth(), new GoogleAuthProvider())
-    .then((result) => {
-      onAuth(result.user);
-    }).catch((error) => {
-      console.error("Сталася помилка, спробуйте, будь ласка, ще раз.")
+    .then(() => {
+      console.log("Sign In successful.");
+    })
+    .catch((error) => {
+      console.error("Сталася помилка, спробуйте, будь ласка, ще раз.");
+      console.error(error.message);
     });
 }
 
 export function userSignOut() {
-  signOut(getAuth()).then(() => {
-    console.log("Sign-out successful.");
-  }).catch((error) => {
-    console.log("Sign-out UNsuccessful.");
-  });
+  signOut(getAuth())
+    .then(() => {
+      console.log("Sign-out successful.");
+    })
+    .catch((error) => {
+      console.error("Sign-out unsuccessful.");
+      console.error(error.message);
+    });
 }
